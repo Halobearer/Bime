ab2d5d66e201e3ffc45e0773fb889ee217d259d7
Damian <ndebuezedamian@gmail.com>
2023-07-18 23:16:07 +0100

    Match Algorithm Commit

--- ChromeExt/scripts/matchAlgorithm.js
+++ ChromeExt/scripts/matchAlgorithm.js
@@ -2,32 +2,42 @@ document.addEventListener('DOMContentLoaded', function () {
     const websiteMatch = document.querySelector('.host-input');
 
     const websiteData = [
-        {hostname: "www.whatsapp.com", icon: "whatsapp-icon.png"},
-        {hostname: "www.facebook.com", icon: "facebook-icon.png"},
-        {hostname: "www.youtube.com", icon: "youtube-icon.png"},
-        {hostname: "www.google.com", icon: "google-icon.png"},
-        {hostname: "www.twitter.com", icon: "twitter-icon.png"}
+        {hostname: "www.whatsapp.com", icon: "icons/whatsapp-icon.png"},
+        {hostname: "www.facebook.com", icon: "icons/facebook-icon.png"},
+        {hostname: "www.youtube.com", icon: "icons/youtube-icon.png"},
+        {hostname: "www.google.com", icon: "icons/google-icon.png"},
+        {hostname: "www.twitter.com", icon: "icons/twitter-icon.png"}
     ];
 
     websiteMatch.addEventListener('input', function () {
         const hostname = websiteMatch.value;
+        const normalizedHostname = normalizeHostname(hostname);
 
         for (let i = 0; i < websiteData.length; i++) {
-            if (websiteData[i].hostname === hostname) {
+            const websiteHostname = websiteData[i].hostname;
+            if (websiteHostname === normalizedHostname) {
                 const icon = websiteData[i].icon;
                 displayIcon(icon);
-                break;
-            } else {
-                displayIcon(null);
+                return;
             }
         }
+
+        displayIcon(null);
     });
+
+    function normalizeHostname(hostname) {
+        const prefix = 'www.';
+        if (hostname.startsWith(prefix)) {
+            return hostname;
+        }
+        return prefix + hostname;
+    }
 
     function displayIcon(icon) {
         const iconElement = document.getElementById('hostname-icon');
 
         if (icon) {
-            iconElement.style.backgroundImage = `url(${icon})`;
+            iconElement.style.backgroundImage = `url(${chrome.runtime.getURL(icon)})`;
             iconElement.style.display = 'block';
         } else {
             iconElement.style.display = 'none';
